bcda3096f844297745f2031f146e45728deda126
mr-greg <gregory.drv@gmail.com>
2023-12-29 17:12:04 +0100

    animation header

--- src/components/Header/StyledHeader.jsx
+++ src/components/Header/StyledHeader.jsx
@@ -26,6 +26,25 @@ export const StyledHeader = styled.header`
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    text-decoration: none;
+    position: relative;
+  }
+  .song::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    border-radius: 4px;
+    background-color: #ffffff;
+    bottom: 0;
+    left: 0;
+    transform-origin: right;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+  .song:hover::before {
+    transform-origin: left;
+    transform: scaleX(1);
   }
   a {
     color: rgba(255, 255, 255, 0.7);
@@ -36,5 +55,9 @@ export const StyledHeader = styled.header`
     line-height: normal;
     text-decoration: none;
     text-transform: uppercase;
+    transition: ease-in-out 0.2s;
+  }
+  :hover {
+    color: white;
   }
 `;
